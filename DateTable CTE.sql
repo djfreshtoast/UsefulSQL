c957669a7f8227fb3f06ff72505ee45e85af7f90
djfreshtoast <djfreshtoast@gmail.com>
2016-07-22 12:52:58 -0600

    Update DateTable CTE.sql
    Updated

--- DateTable CTE.sql
+++ DateTable CTE.sql
@@ -1,13 +1,18 @@
+DECLARE @startdate CHAR(8), @numberDays TINYINT
+
+SET @startdate = '20160101'
+SET @numberDays = 10;
+
 WITH CTE_DatesTable
 AS
 (
-  SELECT CAST('20150101' as date) AS [date]
+  SELECT CAST(@startdate as date) AS [date]
   UNION ALL
   SELECT DATEADD(dd, 1, [date])
   FROM CTE_DatesTable
-  WHERE DATEADD(dd, 1, [date]) <= GetDate()
+  WHERE DATEADD(dd, 1, [date]) <= DateAdd(DAY, @numberDays-1, @startdate)
 )
 
-SELECT * FROM CTE_DatesTable
+SELECT [date] FROM CTE_DatesTable
 
 OPTION (MAXRECURSION 0)
