f31bd013760234b30c59cd350aba314fe6aac48c
djfreshtoast <djfreshtoast@gmail.com>
2016-07-22 15:27:47 -0600

    Add files via upload

--- troubleshoot_memory.sql
+++ troubleshoot_memory.sql
@@ -1,12 +1,14 @@
-EXEC sp_whoisactive
-
 SELECT * FROM sys.dm_exec_query_memory_grants
 SELECT * FROM sys.dm_exec_query_resource_semaphores
 
-SELECT 'Page Life Expectancy' AS 'Metric', [cntr_value] AS 'Value', 'If this value is below ~500 and not growing, there may be memory issues on the server. This value indicates the average length in seconds that a page remains in the buffer pool. The longer the better.' AS 'Description'
+SELECT 'Page Life Expectancy (Sec)' AS 'Metric', 
+			[cntr_value] AS 'Value'
 FROM sys.dm_os_performance_counters
-WHERE [object_name] LIKE '%Manager%' AND [counter_name] = 'Page life expectancy'
-UNION SELECT
-'# Blocked Queries' AS 'Metric', Count(*) AS 'Value', 'Any non-0 # here is a bad thing. This is the number of queries that are currently blocked by another query. Run EXEC sp_whoisactive.' AS 'Description' 
+WHERE [object_name] LIKE '%Manager%' 
+		   AND [counter_name] = 'Page life expectancy'
+UNION SELECT '# Blocked Queries' AS 'Metric', 
+					  Count(*) AS 'Value'
 FROM sys.dm_exec_requests
-WHERE blocking_session_id IS NOT NULL AND sql_handle IS NOT NULL AND blocking_session_id <> 0
+WHERE blocking_session_id IS NOT NULL 
+		   AND sql_handle IS NOT NULL 
+		   AND blocking_session_id <> 0
