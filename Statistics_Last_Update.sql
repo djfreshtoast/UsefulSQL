3a717ad5a621bab62d0951b4b15df779f787fa50
djfreshtoast <djfreshtoast@gmail.com>
2016-07-22 15:15:12 -0600

    Add files via upload

--- Statistics_Last_Update.sql
+++ Statistics_Last_Update.sql
@@ -1,5 +1,9 @@
-SELECT s.name, s.stats_id, s.object_id, STATS_DATE(s.object_id, s.stats_id) AS 'Update Date', o.name AS 'Table'
+SELECT s.name, 
+			s.stats_id, 
+			s.object_id, 
+			STATS_DATE(s.object_id, s.stats_id) AS 'Update Date', 
+			o.name AS 'Table'
 FROM sys.stats s
 INNER JOIN sys.objects o ON o.object_id = s.object_id
-WHERE o.name IN ('MER_Terminal', 'MER_DBA', 'MER_CARD', 'GEN_ASSOCIATION', 'GEN_APPLICATION_ATTRIBUTE', 'GEN_STW_NAME', 'MER_EQUIPMENT_DETAIL')
-ORDER BY o.name ASC, s.stats_id ASC
+WHERE o.name IN ('employee_info')
+ORDER BY Stats_Date(s.object_id, s.stats_id) ASC
