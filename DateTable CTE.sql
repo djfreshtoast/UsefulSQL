b59463c67dc95a7d2756b2319a78f2f098de0b80
djfreshtoast <djfreshtoast@gmail.com>
2016-07-22 15:11:39 -0600

    Add files via upload

--- DateTable CTE.sql
+++ DateTable CTE.sql
@@ -1,18 +1,19 @@
-DECLARE @startdate CHAR(8), @numberDays TINYINT
+DECLARE @startDate DATE, @numberDays TINYINT
 
-SET @startdate = '20160101'
+SET @startDate = '2016-01-01'
 SET @numberDays = 10;
 
 WITH CTE_DatesTable
 AS
 (
-  SELECT CAST(@startdate as date) AS [date]
+  SELECT @startDate AS [date]
   UNION ALL
-  SELECT DATEADD(dd, 1, [date])
+  SELECT DATEADD(DAY, 1, [date])
   FROM CTE_DatesTable
-  WHERE DATEADD(dd, 1, [date]) <= DateAdd(DAY, @numberDays-1, @startdate)
+  WHERE DATEADD(DAY, 1, [date]) <= DateAdd(DAY, @numberDays-1, @startdate)
 )
 
-SELECT [date] FROM CTE_DatesTable
+SELECT [date] 
+FROM CTE_DatesTable
 
 OPTION (MAXRECURSION 0)
